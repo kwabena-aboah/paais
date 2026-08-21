039c60454668ef697132fd22426ca202a29f6996
kwabena-aboah <obed30mintah@yahoo.com>
2026-08-21 12:23:35 +0000

    Fixed django python path for vercel

--- paais_academy/api/index.py
+++ paais_academy/api/index.py
@@ -1,24 +1,18 @@
 import os
 import sys
 
-# Get the root directory of the deployed project
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Path to the Django project directory
 PROJECT_DIR = os.path.join(BASE_DIR, "paais_academy")
 
-# Make Django project importable
 if PROJECT_DIR not in sys.path:
     sys.path.insert(0, PROJECT_DIR)
 
-# Tell Django which settings module to use
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "paais_academy.settings"
 )
 
-# Load Django
 from django.core.wsgi import get_wsgi_application
 
-# Vercel looks for this callable
 app = get_wsgi_application()
