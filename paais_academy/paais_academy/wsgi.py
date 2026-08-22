7b25fe86a15b11d0e44efb3e612e81c2e83eb235
kwabena-aboah <obed30mintah@yahoo.com>
2026-08-22 22:22:20 +0000

    Allow for international phonenumber verifications

--- paais_academy/paais_academy/wsgi.py
+++ paais_academy/paais_academy/wsgi.py
@@ -1,14 +1,13 @@
 import os
 import sys
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(
-        os.path.abspath(__file__)
-    )
-)
+# Project directory:
+# /paais_academy/
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, BASE_DIR)
+if str(BASE_DIR) not in sys.path:
+    sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
